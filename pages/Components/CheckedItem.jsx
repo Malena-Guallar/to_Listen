1370735473c7b80db9df616e616c9a89c048c132
justinelambert12 <justine-lambert@hotmail.fr>
2023-09-06 12:41:00 +0200

    Delete MusicItem from database when button delete clicked for CheckedItem

--- pages/Components/CheckedItem.jsx
+++ pages/Components/CheckedItem.jsx
@@ -2,26 +2,42 @@ import { React } from "react";
 import { useDispatch } from "react-redux";
 import { deleteFromList } from "../../Redux/List.slice";
 
-
 function CheckedItem({ todo }) {
-
   const dispatch = useDispatch();
 
   const handleDelete = () => {
+    deleteFromDatabase(todo.id);
     dispatch(deleteFromList(todo.id));
-  }
+  };
 
-    return (
-        <div key={todo.id} className="flex items-center">
-        <p id="coral_square" className="bg-coral p-5 text-coral max-w-fit">a</p>
-        <p className="flex flex-row pl-5 w-full justify-between">
-            <p>{todo.text}</p>
-          <button onClick={handleDelete} className=" text-coral font-semibold flex pr-8">
-            delete
-          </button>
-        </p>
-      </div>
-    )
+  const deleteFromDatabase = async (id) => {
+    const url = "http://localhost:3000/api/musicItem/" + id;
+    const response = await fetch(url, {
+      method: "DELETE",
+      body: JSON.stringify({ id: id }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((response) => response.json());
+    console.log(response.message);
+  };
+
+  return (
+    <div key={todo.id} className="flex items-center">
+      <p id="coral_square" className="bg-coral p-5 text-coral max-w-fit">
+        a
+      </p>
+      <p className="flex flex-row pl-5 w-full justify-between">
+        <p>{todo.text}</p>
+        <button
+          onClick={handleDelete}
+          className=" text-coral font-semibold flex pr-8"
+        >
+          delete
+        </button>
+      </p>
+    </div>
+  );
 }
 
 export default CheckedItem;
